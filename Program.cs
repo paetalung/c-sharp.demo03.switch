7d6dcac881d948e267e3e8cdfd2b3febac508faf
paetalung <paetalung@gmail.com>
2019-06-08 17:19:56 +0700

    change output

--- Program.cs
+++ Program.cs
@@ -1,12 +1,32 @@
 ﻿using System;
 
-namespace demo03.switch
+namespace demo03.switchCase
 {
     class Program
-{
-    static void Main(string[] args)
     {
-        Console.WriteLine("Hello World!");
+            static void Main(string[] args)
+            {
+                int money = 0;
+                string gender = "male";
+
+            Console.WriteLine("What is your gender? male/female");
+            gender = Console.ReadLine();
+
+                switch (gender)
+                {
+                    case "male":
+                        money += 10;
+                        break;
+                    case "female":
+                        money += 20;
+                        break;
+
+                    default:
+                        break;
+                }
+            Console.WriteLine("You are "+gender+".");
+            Console.WriteLine("You get " + money + " from us.");
+        }
     }
 }
-}
+
